9baf5941827e74c0e9a35151262f8b7616694f7f
Carlos Rodriguez <carlos.amodri@gmail.com>
2020-06-22 10:39:06 -0300

    Reparacion, funciona :nav-carrousel-listado-ofertas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { MatDialog, MatDialogConfig,MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { ListHomeDialogComponent } from './dialog/list-home-dialog/list-home-dialog.component';
+
 
 export interface DialogData{
   nombre : string;
@@ -21,15 +21,6 @@ export class AppComponent {
   title = 'muestraCarn1';
   nombre : string;
 
-  openDialog(nom : string){
-    // const dialogConfig = new MatDialogConfig();
-    // dialogConfig.disableClose = true;
-    // dialogConfig.autoFocus = true;
-    this.nombre = nom;
-    const dialogRef = this.dialog.open(ListHomeDialogComponent,{
-      data : {nombre : this.nombre}
-    });
-    
-  }
+
 
 }
